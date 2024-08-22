9af137ac3265927cdced5a9a5d52393f4ae241d7
Keepingwinner1 <118123621+Keepingwinner1@users.noreply.github.com>
2024-08-22 18:48:14 +0800

    Add files via upload

--- src/mixins/postMixin.js
+++ src/mixins/postMixin.js
@@ -2,38 +2,6 @@ import axios from 'axios';
 
 export const postMixin = {
     methods: {
-        toggleLike(post) {
-            const token = this.$store.state.token; // 从 Vuex 获取 token
-            if (post.liked) {
-                axios.delete('http://localhost:8080/api/PostContoller/CancleLikePost', {
-                    params: { postID: post.postID },
-                    headers: {
-                        Authorization: `Bearer ${token}`
-                    }
-                })
-                    .then(() => {
-                        post.likesCount -= 1;
-                        post.liked = false;
-                    })
-                    .catch(error => {
-                        console.error('取消点赞时发生错误:', error);
-                    });
-            } else {
-                axios.post('http://localhost:8080/api/Post/likePost', null, {
-                    params: { postID: post.postID },
-                    headers: {
-                        Authorization: `Bearer ${token}`
-                    }
-                })
-                    .then(() => {
-                        post.likesCount += 1;
-                        post.liked = true;
-                    })
-                    .catch(error => {
-                        console.error('点赞时发生错误:', error);
-                    });
-            }
-        },
 
         getCommentCount(postID) {
             const token = this.$store.state.token;
